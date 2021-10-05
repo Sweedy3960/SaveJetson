36952880c495fa8964667d6aa5359c98f573fdd2
Aymeric CLAUZEL <Aymeric.CLAUZEL@cpnv.ch>
2021-10-05 20:14:31 +0200

    calib

--- SaveALL/myPy/Testpoo-Capture.py
+++ SaveALL/myPy/Testpoo-Capture.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+#modification d'un pipeline trouvé sur le net pour test de capture 
 class capture :
     def __init__(self) :
         self.capture_width=1680,
@@ -8,16 +9,10 @@ class capture :
         self.display_height=500,
         self.framerate=10,
         self.flip_method=2,
+    def setCap(self):
+        self.capture_width=int(input("Largeur en pixels SVP :"))
+        self.capture_height=int(input("hauteur en pixels SVP :"))
     def gstreamer_pipeline(self):
-        self.capture_width=1680
-        self.capture_height=1050
-        self.display_width=500
-        self.display_height=500
-        self.framerate=10
-        self.flip_method=2
-    def getvalue(self):
-        self.capture_width=int(input("hauteur svp : "))
-        self.capture_height=int(input("hauteur svp : "))
         return (
             "nvarguscamerasrc ! "
             "video/x-raw(memory:NVMM), "
@@ -35,7 +30,27 @@ class capture :
                 self.display_width,
                 self.display_height,
             )
-    )
+        )
 if __name__ == "__main__":
     capture1=capture()
+    img_cnt = 0
+    while True:
+        mod = input("voullez vous modifier les dimensions de capture? y/n")
+        if mod == "y":
+            capture1.setCap(capture1)
+        rdy = input("Voullez vous enregistrer une image ? y/n")
+        if rdy == "y":
+            img = cv.VideoCapture(capture1.gstreamer_pipeline(capture1),cv.CAP_GSTREAMER)
+            ret,frame = img.read()
+            #DEBUG/VISUAL
+            #cv.imshow("test", frame)
+            png_name = "opencv_frame_{}.png".format(img_cnt)
+            cv.imwrite(png_name, frame)
+            print("{} written!".format(png_name))
+            img_cnt+=1
+        #------Pour quitter "q"---------
+        if cv.waitKey(1) & 0xFF == ord('q'):
+         break
+
+cv.destroyAllWindows()
     
