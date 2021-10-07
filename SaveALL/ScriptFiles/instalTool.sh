b01c0024bb24018149f7f8c8abb2d7014f5ead07
Sweed3960 <aymeric.clauzel@outlook.fr>
2021-10-07 08:23:07 +0200

    yep

--- SaveALL/ScriptFiles/instalTool.sh
+++ SaveALL/ScriptFiles/instalTool.sh
@@ -1,16 +1,9 @@
-sudo apt-get update
-sudo apt-get upgrade
 
-sudo apt-get install nano 
-print("nano ok")
-sudo apt-get install python3
-print("python3 ok")
-sudo apt-get install mc 
-print("mc ok")
-sudo apt-get install htop 
-print("htop ok")
 
 sudo apt-get install curl
 print("curl ok")
+cd ~/Téléchargements
+curl -L https://github.com/toolboc/vscode/releases/download/1.32.3/code-oss_1.32.3-arm64.deb -o code-oss_1.32.3-arm64.deb
 
+sudo dpkg -i code.oss_1.32.3-arm64.deb
 print("good work")
