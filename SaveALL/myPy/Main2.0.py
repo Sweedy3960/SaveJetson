cdaf72e1884e7689f144b8077bc6d8edce1bc97b
Sweed3960 <aymeric.clauzel@outlook.fr>
2021-10-08 23:36:13 +0200

    main2.0 ok

--- SaveALL/myPy/Main2.0.py
+++ SaveALL/myPy/Main2.0.py
@@ -8,7 +8,13 @@ calib_path="SaveALL/myFi/"
 CAMERA_MATRIX = np.loadtxt(calib_path+'cameraMatrix.txt', delimiter=',')
 DIST_COEFFS  = np.loadtxt(calib_path+'cameraDistortion.txt', delimiter=',')
 #---------------------------------------------------------------
-
+#GST_ARGUS: Available Sensor modes :
+#GST_ARGUS: 3264 x 2464 FR = 21.000000 fps Duration = 47619048 ; Analog Gain range min 1.000000, max 10.625000; Exposure Range min 13000, max 683709000;
+#GST_ARGUS: 3264 x 1848 FR = 28.000001 fps Duration = 35714284 ; Analog Gain range min 1.000000, max 10.625000; Exposure Range min 13000, max 683709000;
+#GST_ARGUS: 1920 x 1080 FR = 29.999999 fps Duration = 33333334 ; Analog Gain range min 1.000000, max 10.625000; Exposure Range min 13000, max 683709000;
+#GST_ARGUS: 1640 x 1232 FR = 29.999999 fps Duration = 33333334 ; Analog Gain range min 1.000000, max 10.625000; Exposure Range min 13000, max 683709000;
+#GST_ARGUS: 1280 x 720 FR = 59.999999 fps Duration = 16666667 ; Analog Gain range min 1.000000, max 10.625000; Exposure Range min 13000, max 683709000;
+#GST_ARGUS: 1280 x 720 FR = 120.000005 fps Duration = 8333333 ; Analog Gain range min 1.000000, max 10.625000; Exposure Range min 13000, max 683709000;
 #modification d'un pipeline trouvé sur le net pour test de capture 
 class capture :
     def __init__(self):
@@ -40,30 +46,28 @@ class capture :
     def setCap(self):
         self.capture_width=int(input("Largeur en pixels SVP :"))
         self.capture_height=int(input("hauteur en pixels SVP :"))
+
 class imgProcess :
     def __init__(self):
         self.corners=[]
         self.ids=[]
         self.rejctedImgPoints=[]
-        
         self.ret=[]
         self.img = cv.VideoCapture(capture1.gstreamer_pipeline(), cv.CAP_GSTREAMER)
-        self.a = []
-    def imgprocess(self) :
+        self.infoMarkers = []
+    def imgwork(self) :
         self.ret,self.frame = self.img.read()
         self.gray = cv.cvtColor(self.frame,cv.COLOR_BGR2GRAY)
+        self.infoMarkers = cv.aruco.detectMarkers(self.gray,DICTIONARY,parameters = PARAMETERS)
+        for i in self.infoMarkers[0]:
+            self.rvecs, self.tvecs, markerPoints= cv.aruco.estimatePoseSingleMarkers(i,MARKER_EDGE, CAMERA_MATRIX, DIST_COEFFS)
+            #DEBUG
+            #print(img.rvecs)
+            #self.frame = cv.aruco.drawAxis(self.frame, CAMERA_MATRIX, DIST_COEFFS, self.rvecs, self.tvecs,0.10)
+        #debug
+        #self.frame = cv.aruco.drawDetectedMarkers(self.frame, self.infoMarkers[0],self.infoMarkers[1])
+        #cv.imshow("that",self.frame)
 
-        self.a = cv.aruco.detectMarkers(self.gray,DICTIONARY,parameters = PARAMETERS)
-        self.frame = cv.aruco.drawDetectedMarkers(self.frame, self.a[0],self.a[1])
-        cv.imshow("that",self.frame)
-
-    
-    def getcorns(self):
-        return(self.corners)
-    def getids(self):
-        return(self.ids)
-    def getframe(self):
-        return(self.frame)
 
 if __name__ == "__main__":
     
@@ -71,8 +75,8 @@ if __name__ == "__main__":
     img=imgProcess()
     Dict_markers = {}
     while True:
-        img.imgprocess()
-        print(img.a)
+        img.imgwork()
+  
         #------Pour quitter "q"---------
         if cv.waitKey(1) & 0xFF == ord('q'):
             img.img.release()
