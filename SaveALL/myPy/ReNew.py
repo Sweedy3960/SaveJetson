77780826867d8c451881974f9ee609a872cc1c1f
Sweedy3960 <aymeric.clauzel@outlook.fr>
2021-12-09 16:08:48 +0100

    ok

--- SaveALL/myPy/ReNew.py
+++ SaveALL/myPy/ReNew.py
@@ -14,6 +14,20 @@ class Vector2:
         return Vector2(self.x - other.x, self.y - other.y)
     def __str__(self) -> str:
         return f"({self.x}, {self.y})"
+class Vector3:
+        def __init__(self, x, y, z) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+    def norme(self):
+        return (self.x ** 2 + self.y ** 2 + self.z ** 2) ** 0.5
+    def __add__(self, other):
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+    def __sub__(self, other):
+        return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+    def __str__(self) -> str:
+        return f"Vector3({self.x}, {self.y}, {self.z})"
+
 
 class Capture :
     def __init__(self):
@@ -50,8 +64,8 @@ class App:
     MARKER_EDGE = 0.07
     MARKER_MIDL = 0.10
     MIDL=42
-    SAMPLES_T=[]
-    ROBOTS_T=[]
+    SAMPLES_T=[47,13,36,17]
+    ROBOTS_T=[1,2,3,4,5,6,7,8,9,10]
     MARKER_SAMPLE=0.05
     calib_path="SaveALL/myFi/"
     CAMERA_MATRIX = np.loadtxt(calib_path+'intrinsic30.11.txt', delimiter=',')  
@@ -85,13 +99,14 @@ class ImProc:
         self.netFil=[]
         self.ListId=[]
         self.found=[]
+        self.detected =False
         self.tagin={}
         self.Debug=False
         for i in cap:
             self.frame.append(None)
             self.cap.append(cv.VideoCapture(i.gstreamer_pipeline(),cv.CAP_GSTREAMER))
             self.infoMarkers.append(None)  
-            self.ListId.append(None)
+            self.ListId.append([])
             self.gray.append(None)
             self.gauss.append(None)
             self.trhesh.append(None)
@@ -138,22 +153,24 @@ class ImProc:
     def Detect(self):
         for i,j in enumerate(self.gray):
             self.infoMarkers[i]=cv.aruco.detectMarkers(j, App.DICTIONARY, parameters = App.PARAMETERS)
-            print(self.infoMarkers[i])
-
+            #print(self.infoMarkers[i])
+            try:
+                if self.infoMarkers[i][1] == None:
+                    return 0
+            except:
+                return 1
     def SortName(self):
         #infomarker [corners][id][rejected points]
         for i,j in enumerate(self.infoMarkers):
-            if not None in j[1]:
-                #print("PAS NONE")
-                for l,k in enumerate(self.infoMarkers[i][1]):
+            for l,k in enumerate(self.infoMarkers[i][1]):
                    # print(k)
-                    print(self.infoMarkers[i][1][l])
+                    #print(self.infoMarkers[i][1][l])
                     #faire remplacement sur une ligne puis remplacer 
-                    self.infoMarkers[i][1][l]=int(str(self.infoMarkers[i][1][l]).replace("[",""))
-                    self.infoMarkers[i][1][l]=int(str(self.infoMarkers[i][1][l]).replace("]",""))
-                    print(self.infoMarkers[i][1][l])
-                   
-                   
+                a=(str(self.infoMarkers[i][1][l]).replace("[",""))
+                a=a.replace("]","")
+                    #print(a) 
+                self.ListId[i].append(int(a))
+                    #print(self.ListId)
                    # m=str(k)
                    # #k=m[1:-1]
                    # k=m.replace("["," ")
@@ -164,16 +181,34 @@ class ImProc:
             #self.found[i]=len(i[1])  
               
     def TriTag(self):
-        for i in self.infoMarkers:
-            if self.infoMarkers[0][1] !=None:
-                for j in self.infoMarkers[1]:
-                    self.tagin["tag{}".format(j)] = Tag(i,int(j),self.infoMarkers[0][j])
+        for i,j in enumerate(self.ListId):
+            for k,l in enumerate(j):
+                self.tagin["tag{}".format(l)] = Tag(self.infoMarkers[i][0],int(l),int(k))
         return self.tagin
     
+    def GetPos(self):
+        a=list(self.tagin.keys())
+        #print(a)
+        if "tag"+str(App.MIDL) in a:
+            for i in a:
+                #print("42detected")
+                #print(self.tagin["tag42"].vect2d)
+                #print(self.tagin["{}".format(i)].vect2d)
+                z=(self.tagin["tag42"].vect2d)-(self.tagin["{}".format(i)].vect2d)
+                print(z)
+                o=Vector2.norme(z)
+                print(0)
+                
+            #print("{}".format(i)+str(self.tagin[i].getpos()))
+            
 
     def Release_All(self):
         for i in self.cap:
             i.release()
+    def TagWork(self):
+        self.SortName()
+        self.TriTag()
+        self.GetPos()
     def FrameWorking(self):
         self.ToGray()
         #self.Gauss()
@@ -182,9 +217,9 @@ class ImProc:
     def Update(self):
         self.ReadFrames()
         self.FrameWorking()
-        self.Detect()
-        self.SortName()
-        print(self.TriTag())
+        if self.Detect():
+            self.TagWork()
+    
        
         #save Dictio {ID}:infomarker(propre à la caméra qui a détécté le tag)
         #save=self.TriTag()
@@ -192,25 +227,37 @@ class ImProc:
          
 
 class Tag:
-    def __init__(self,infoMarkers:list,whoami:int,corners):
-        
-        if whoami == App.MIDL:
-            self.pos=(1450,1200)
+    def __init__(self,infoMarkers:list,whoami:int,index:int):
+        self.posRe=(0,0)
+        self.Facing=0
+        self.Id=whoami
+        self.tvecs=None
+        self.rvecs=None
+        self.corn=infoMarkers
+        self.vect2d=None
+        self.diffX=0
+        self.diffY=0 
+        self.debug=True
+        if self.Id == App.MIDL:
+            self.posRe=(1450,1200)
             self.marker_edge=0.1
         elif whoami == App.ROBOTS_T:
             self.marker_edge=0.07
         else:
             self.marker_edge=0.05
         
-        self.Id=whoami
-        self.tvecs=None
-        self.rvecs=None
-        self.corn=corners
+        self.update()
     def FindV(self):
         self.rvecs, self.tvecs, markerPoints= cv.aruco.estimatePoseSingleMarkers(self.corn,self.marker_edge, App.CAMERA_MATRIX, App.DIST_COEFFS)
+        #return(self.rvecs,self.tvecs)
     def FindD(self):
-        print(round(100 * self.calcul_dist2(0), 2), "cm")
-
+        self.vect2d= Vector2(self.tvecs[0][0][0],self.tvecs[0][0][1])
+    def update(self):
+        self.FindV()
+        self.FindD()
+        if self.debug:
+            print("{}".format(self.Id)+"{}".format(self.vect2d))
+    
 def main() -> int:
     app1 = App()
     app1.main()
