58de304d22aef2560efedd0e786ca80a133da4c2
Aymeric CLAUZEL <Aymeric.CLAUZEL@outlook.fr>
2022-03-17 21:36:42 +0100

    ok>

--- SaveALL/myPy/ServerTcpIp/client.py
+++ SaveALL/myPy/ServerTcpIp/client.py
@@ -4,59 +4,20 @@
 # Raspberry à utiliser: Client
 
 #importation des librairies
-import time
-import RPi.GPIO as GPIO
+
+
 import socket
 import os
-
-# init des GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11,GPIO.OUT)                           # Voyant du raspberry
-GPIO.setup(7,GPIO.OUT)                            # Voyant état du client   
-GPIO.setup(31,GPIO.IN)                            # Interrupteur
-GPIO.setup(37,GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Bouton d'interruption
-GPIO.setup(40,GPIO.IN)                            # Récepteur infrarouge
-
-# Indique que le programme a démarré
-GPIO.output(7,GPIO.HIGH)
-
-# Fonction d'interruption
-def Turn_Off():
-    # Éteint voyant d'état du raspberry
-    GPIO.output(7,GPIO.LOW)
-    # Attente 2 secondes
-    time.sleep(2)
-    # Éteignage du raspberry
-    os.system("sudo shutdown -h now")
-
-GPIO.add_event_detect(37, GPIO.FALLING, callback=Turn_Off, bouncetime=300)
-
-#variable globale
-BOUTON = 0
-Start = 0
+import time
 
 # adresse IP du serveur et port de communication
 hote = "10.3.141.1"
 port = 12800
 
-# Tant que le bouton est $ 0 fait clignoter la LED
-while Start == 0:
-    Start = GPIO.input(31)
-    GPIO.output(11,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(11,GPIO.LOW)
-    time.sleep(1)
-    
-GPIO.output(11,GPIO.LOW)
 #Tentative de connexion au serveur
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 conn.connect((hote, port))
 print("Connexion établie avec le serveur sur le port {}".format(port))
-
-#allumage de la LED
-GPIO.output(11,GPIO.HIGH)
-
 #init des variables locales
 memoire = 0 # mémoire de l'état de la barrière optique
 Msg = "New"
@@ -64,38 +25,12 @@ Msg = Msg.encode()
 conn.send(Msg)
 time.sleep(10)
 Msg = "Next"
-Msg = Msg.encode()
-#tant que le bouton start est actif
-while Start !=0:
-    #Lecteur du bouton start
-    Start = GPIO.input(31)
-    #sauvegarde de l'état de la barrière optique
-    memoire = BOUTON
-    #Lecture de la barrière optique
-    BOUTON = GPIO.input(40)
-    #Test si la barrière optique est coupé
-    if(BOUTON == 0):
-        #éteind la LED
-        GPIO.output(11,GPIO.LOW)
-    else:
-        #allume la LED
-        GPIO.output(11,GPIO.HIGH)
-        #Test si la barrière optique est coupée
-        if((BOUTON == 1)and(memoire == 0)):
-            conn.send(Msg)
-            print("Envoyé")
-            # attente de 1sec avant de pouvoir relire
-            # la valeur de la barrière optique
-            time.sleep(1)
-            
+Msg = Msg.encode()       
 # si le bouton start = 0 envoie du message fin qui coupe la connexion
 # du serveur et du client
 Msg = "Off"
 Msg = Msg.encode()
 conn.send(Msg)
-
 print("close")
-GPIO.output(7,GPIO.LOW)
 print("Fermeture de la connexion")
-#conn.close()
-Turn_Off()
+conn.close()
--- SaveALL/myPy/ServerTcpIp/serveur.py
+++ SaveALL/myPy/ServerTcpIp/serveur.py
@@ -1,54 +1,10 @@
-#///////Serveur///////
-#Programe: Serveur.py
-#Programmeur : Arthur Phung
-# Date:02.12.2020
 
 #importation des librairy
-import RPi.GPIO as GPIO
 import socket
 import select
-import os
-import time
-import pyautogui
-
-#init des ports de sorties
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(3,GPIO.OUT)
-GPIO.setup(5,GPIO.OUT)
-GPIO.setup(7,GPIO.OUT)
-GPIO.setup(37, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-#indique que le programme a démarré
-GPIO.output(3,GPIO.HIGH)
-# attente du chargement du bureau
-time.sleep(45)
-GPIO.output(5,GPIO.HIGH)
-#Démarrage du powerpoint
-try:
-	os.system("open /media/pi/*/*.odp")
-	time.sleep(200)
-	print("office run")
-	pyautogui.press('F5')
-except:
-    	print("erreur 404: dossier pas trouver")
-
-#Programmation de l'interruption d'éteignage
-def my_callback():
-   pyautogui.press('esc')
-   time.sleep(3)
-   pyautogui.hotkey('ctrl','s')
-   time.sleep(20)
-   pyautogui.hotkey('alt','F4')
-   time.sleep(60)
-   os.system("shutdown now -h") #éteind le système
-
-GPIO.add_event_detect(37, GPIO.FALLING, callback=my_callback, bouncetime=300)
-
 #init du port de communication est ip
 hote = ''
 port = 12800
-
 #tentative de connexion
 try:
     connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -57,9 +13,7 @@ try:
     print("Le serveur écoute à présent sur le port {}".format(port)) 
 except:
     print("erreur: port déjà ouvert")
-#si le serveur est connecté allume la deuxième led
-GPIO.output(5,GPIO.HIGH)
-  
+
 # Tant que le serveur ne reçoit pas le message de fin
 serveur_lance = True
 clients_connectes = []
@@ -97,12 +51,11 @@ while serveur_lance:
             msg_recu = msg_recu.decode()
             if (msg_recu == "New") | (msg_recu == "Next") | (msg_recu == "Off"):
                 #allume la troisième reçoit un message
-                GPIO.output(7,GPIO.HIGH)
+                
                 print("Reçu {}".format(msg_recu))
             
                 # si le message fin est détecté
                 if msg_recu == "Next":
-                   pyautogui.press('space')
                    print("esapce")
                 else:
                     if msg_recu == "New":
@@ -112,15 +65,11 @@ while serveur_lance:
                             ClientConn -= 1
                             if ClientConn == 0:
                                 serveur_lance = False
-                GPIO.output(7,GPIO.LOW)
+          
     msg_recu = " "
                 
 # fin de connexion et remise à 0 des ports de sorties
 print("Fermeture des connexions")
 for client in clients_connectes:
     client.close()
-GPIO.output(3,GPIO.LOW)
-GPIO.output(5,GPIO.LOW)
-GPIO.output(7,GPIO.LOW)
 connexion_principale.close()
-my_callback()
